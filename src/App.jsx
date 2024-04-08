7531f9f9eab688f8e22f0a8f4985adbf7c93a080
mslysz <macslysz@gmail.com>
2024-04-09 00:49:17 +0100

    Added functions to control the movement of slides forward or backward.

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,41 @@ import { MdArrowBackIos } from 'react-icons/md';
 import { MdArrowForwardIos } from 'react-icons/md';
 
 const App = () => {
-  const [person, setPerson] = useState(reviews.id);
+  const [index, setIndex] = useState(0);
 
+  const nextPerson = () => {
+    if (index === reviews.length - 1) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+  };
+  const prevPerson = () => {
+    if (index === 0) {
+      setIndex(reviews.length - 1);
+    } else {
+      setIndex(index - 1);
+    }
+  };
   return (
     <main>
       <h2>Reviews Project</h2>
       <section className='review'>
         <div className='img-container'>
-          <img className='person-img' src={reviews[0].image} alt='person-img' />
+          <img
+            className='person-img'
+            src={reviews[index].image}
+            alt='person-img'
+          />
         </div>
-        <h4 className='author'>{reviews[0].name}</h4>
-        <h3 className='job'>{reviews[0].job}</h3>
-        <p className='info'>{reviews[0].text}</p>
+        <h4 className='author'>{reviews[index].name}</h4>
+        <h3 className='job'>{reviews[index].job}</h3>
+        <p className='info'>{reviews[index].text}</p>
         <div className='btn-container'>
-          <button className='prev-btn'>
+          <button className='prev-btn' onClick={prevPerson}>
             <MdArrowBackIos />
           </button>
-          <button className='next-btn'>
+          <button className='next-btn' onClick={nextPerson}>
             <MdArrowForwardIos />
           </button>
         </div>
