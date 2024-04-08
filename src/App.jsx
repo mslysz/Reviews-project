479b3a4112093b2fc66bd0e1f13727e0a01509bb
mslysz <macslysz@gmail.com>
2024-04-08 23:49:53 +0100

    Created a review card structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,32 @@
+import { useState } from 'react';
+import reviews from './data';
+import { MdArrowBackIos } from 'react-icons/md';
+import { MdArrowForwardIos } from 'react-icons/md';
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  const [person, setPerson] = useState(reviews.id);
+
+  return (
+    <main>
+      <h2>Reviews Project</h2>
+      <section className='review'>
+        <div className='img-container'>
+          <img className='person-img' src={reviews[0].image} alt='person-img' />
+        </div>
+        <h4 className='author'>{reviews[0].name}</h4>
+        <h3 className='job'>{reviews[0].job}</h3>
+        <p className='info'>{reviews[0].text}</p>
+        <div className='btn-container'>
+          <button className='prev-btn'>
+            <MdArrowBackIos />
+          </button>
+          <button className='next-btn'>
+            <MdArrowForwardIos />
+          </button>
+        </div>
+        <button className='btn'>Suprise me</button>
+      </section>
+    </main>
+  );
 };
 export default App;
